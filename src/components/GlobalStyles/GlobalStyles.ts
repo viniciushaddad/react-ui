5ae442e4ca31e027bced808a09430a4037775256
Vinicius Haddad <vinicius.haddad@creditas.com.br>
2020-08-07 14:17:04 -0300

    Added font faces to global styles. Fixed input paddings. Added Headers styled component

--- src/components/GlobalStyles/GlobalStyles.ts
+++ src/components/GlobalStyles/GlobalStyles.ts
@@ -1,29 +1,52 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
-// import { GudeaRegular, DidactGothicRegular, MonstserratAlternatesRegular } from '../../../public/fonts/fonts'
+import GudeaRegular from 'fonts/GudeaRegular.ttf'
+import Mulish from 'fonts/Mulish.ttf'
+import Roboto from 'fonts/RobotoRegular.ttf'
+import RobotoCondensed from 'fonts/RobotoCondensedRegular.ttf'
+import BalooTamma2 from 'fonts/BalooTamma2-Regular.ttf'
+import MonstserratAlternatesRegular from 'fonts/MontserratAlternatesRegular.ttf'
 
 const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: Gudea;
-    src: local('Gudea'), url('fonts/Gudea/Gudea-Bold.ttf') format('ttf');
+    src: url(${GudeaRegular});
     font-weight: 300;
     font-style: normal;
   }
   @font-face {
-    font-family: 'Didact Gothic';
-    src:  local('Didact Gothic'), local('DidactGothic'), url('fonts/Didact_Gothic/Didact_Gothic-Regular.tff) format('ttf');
+    font-family: 'Mulish';
+    src: url(${Mulish});
     font-weight: 300;
     font-style: normal;
   }
   @font-face {
-    font-family: 'Montserrat Alt';
-    src: local('MontserratAlt'), local('Montserrat Alt'), url('fonts/Montserrat_Alternates/MontserratAlternates-Regular.ttf') format('ttf');
+    font-family: 'Roboto';
+    src: url(${Roboto});
+    font-weight: 300;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Roboto Condensed';
+    src: url(${RobotoCondensed});
+    font-weight: 300;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Montserrat Alternates';
+    src: url(${MonstserratAlternatesRegular});
+    font-weight: 300;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Baloo Tamma 2';
+    src: url(${BalooTamma2});
     font-weight: 300;
     font-style: normal;
   }
 
   body {
-    font-family: Gudea, Nunito, Roboto, sans-serif;
+    font-family: Mulish, Roboto, sans-serif;
   }
 `
 
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -6,11 +6,8 @@ const InputWrapper = styled.div`
 `
 const Label = styled.label`
   font-size: 1em;
-  font-weight: ${({
-    theme: {
-      font: { weights }
-    }
-  }) => weights.label};
+  font-family: ${({ theme: { font } }) => font.family.titles};
+  font-weight: ${({ theme: { font } }) => font.weights.label};
   color: ${({ theme: { label } }) => label.color};
 `
 
@@ -19,13 +16,34 @@ const Input = styled.input`
   border-style: ${({ theme: { input } }) => input.border.style};
   border-bottom-width: ${({ theme: { input } }) => input.border.width};
   border-bottom-color: ${({ theme: { input } }) => input.border.color};
-  padding: .25em .5em
+  padding: 0.25em 0.5em;
 `
 
 interface TextFieldProps {
   label: string
-  type: 'button' | 'checkbox' | 'color' | 'date' | 'datetime' | 'email' | 'file' | 'hidden' | 'image' | 'month' |
-  'number' | 'password' | 'radio' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week'
+  type:
+    | 'button'
+    | 'checkbox'
+    | 'color'
+    | 'date'
+    | 'datetime'
+    | 'email'
+    | 'file'
+    | 'hidden'
+    | 'image'
+    | 'month'
+    | 'number'
+    | 'password'
+    | 'radio'
+    | 'range'
+    | 'reset'
+    | 'search'
+    | 'submit'
+    | 'tel'
+    | 'text'
+    | 'time'
+    | 'url'
+    | 'week'
   value?: string
   onClick?: (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => void
 }
