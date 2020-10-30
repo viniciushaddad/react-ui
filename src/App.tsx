4a480097f7f337232a2660ee51c93fde119cbffc
Vinicius Haddad <vinicius.haddad@creditas.com.br>
2020-10-30 00:17:12 -0300

    Added icon system examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import { Header } from 'components/Header/Header'
 import { Box } from 'components/Box'
+import { Container } from 'components/Container'
+import { Header } from 'components/Header/Header'
 import { Typography } from 'components/Typography'
 import { ButtonExample, GridExample, IconSystemExample, InputExample } from 'Examples'
 
 const App = (): JSX.Element => (
-  <Box>
-    <Header>
-      <Typography variant="largetitle">Styled Components to compose Basic UIs with themes!</Typography>
-      <Typography variant="title2">Examples below.</Typography>
-    </Header>
+  <Container>
+    <Box>
+      <Header>
+        <Typography variant="largetitle">Styled Components to compose Basic UIs with themes!</Typography>
+        <Typography variant="title2">Examples below.</Typography>
+      </Header>
 
-    <IconSystemExample />
-    <InputExample />
-    <ButtonExample />
-    <GridExample />
-  </Box>
+      <IconSystemExample />
+      <InputExample />
+      <ButtonExample />
+      <GridExample />
+    </Box>
+  </Container>
 )
 
 export default App
--- src/Examples/IconSystemExample.tsx
+++ src/Examples/IconSystemExample.tsx
@@ -1,17 +1,78 @@
 import React from 'react'
 import { Typography } from 'components/Typography'
 import { Row, Col, Grid } from 'components/Grid'
-import { Spinners } from 'components/Icons'
+import { Action, Alert, Navigation, Spinners } from 'components/Icons'
+import { Box } from 'components/Box'
+import { Card } from 'components/Card'
+import { chunk } from 'lodash'
 
 const IconSystemExample = (): JSX.Element => (
   <section>
     <Typography variant="title1">Icon System</Typography>
+    <Typography variant="title2" bordered>
+      Spinners
+    </Typography>
+    <Spinners.Loading variant="pulse" />
+    <Typography variant="title2" bordered>
+      Actions
+    </Typography>
     <Grid>
-      <Row>
-        <Col>
-          <Spinners.Loading variant="pulse" />
-        </Col>
-      </Row>
+      {chunk(Object.entries(Action), 3).map((iconsInRows, index) => (
+        <Row key={`icon-row-${index}`}>
+          {iconsInRows.map(([iconName, Icon], index) => (
+            <Col key={`icon-col-${index}`}>
+              <Card alignment="center">
+                <Box>
+                  <Icon />
+                </Box>
+                <Box padding={['0 2em', '0 1em']}>
+                  <Typography>{`<Action.${iconName} />`}</Typography>
+                </Box>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      ))}
+
+      <Typography variant="title2" bordered>
+        Alerts
+      </Typography>
+      {chunk(Object.entries(Alert), 3).map((iconsInRows, index) => (
+        <Row key={`icon-row-${index}`}>
+          {iconsInRows.map(([iconName, Icon], index) => (
+            <Col key={`icon-col-${index}`}>
+              <Card alignment="center">
+                <Box>
+                  <Icon />
+                </Box>
+                <Box padding={['0 2em', '0 1em']}>
+                  <Typography>{`<Alert.${iconName} />`}</Typography>
+                </Box>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      ))}
+
+      <Typography variant="title2" bordered>
+        Navigations
+      </Typography>
+      {chunk(Object.entries(Navigation), 3).map((iconsInRows, index) => (
+        <Row key={`icon-row-${index}`}>
+          {iconsInRows.map(([iconName, Icon], index) => (
+            <Col key={`icon-col-${index}`}>
+              <Card alignment="center">
+                <Box>
+                  <Icon />
+                </Box>
+                <Box padding={['0 2em', '0 1em']}>
+                  <Typography>{`<Navigation.${iconName} />`}</Typography>
+                </Box>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      ))}
     </Grid>
   </section>
 )
