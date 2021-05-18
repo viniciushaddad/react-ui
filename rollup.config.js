28a94d319a3bbad2da81e9916c71ee7017517484
Vinicius Haddad <vinicius.haddad@creditas.com.br>
2021-05-18 00:11:15 -0300

    Moving fonts to global-styles component

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from 'rollup-plugin-typescript2'
+import url from 'rollup-plugin-url'
 import pkg from './package.json'
 
 export default {
@@ -12,6 +13,15 @@ export default {
       strict: false,
     },
   ],
-  plugins: [typescript()],
+  plugins: [
+    typescript(),
+    url({
+      // by default, rollup-plugin-url will not handle font files
+      include: ['**/*.ttf'],
+      // setting infinite limit will ensure that the files
+      // are always bundled with the code, not copied to /dist
+      limit: Infinity,
+    }),
+  ],
   external: ['react', 'react-dom'],
 }
