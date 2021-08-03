67895c9fecd421781af59c9051e50be37ae03272
Vinicius Haddad <vinicius.haddad@creditas.com.br>
2021-08-03 12:13:38 -0300

    Renaming alert icons

--- lib/components/Icons/Alert/index.tsx
+++ lib/components/Icons/Alert/index.tsx
@@ -8,10 +8,10 @@ import {
 import { faFlag } from '@fortawesome/free-regular-svg-icons'
 import { BaseIcon, IconProps } from '../BaseIcon'
 
-const AlertCircle: React.FC<IconProps> = (props): JSX.Element => <BaseIcon {...props} icon={faExclamationCircle} />
-const AlertTriangle: React.FC<IconProps> = (props): JSX.Element => <BaseIcon {...props} icon={faExclamationTriangle} />
+const Circle: React.FC<IconProps> = (props): JSX.Element => <BaseIcon {...props} icon={faExclamationCircle} />
+const Triangle: React.FC<IconProps> = (props): JSX.Element => <BaseIcon {...props} icon={faExclamationTriangle} />
 const Flag: React.FC<IconProps> = (props): JSX.Element => <BaseIcon {...props} icon={faFlag} />
 const Info: React.FC<IconProps> = (props): JSX.Element => <BaseIcon {...props} icon={faInfoCircle} />
 const Help: React.FC<IconProps> = (props): JSX.Element => <BaseIcon {...props} icon={faQuestionCircle} />
 
-export { AlertCircle, AlertTriangle, Flag, Info, Help }
+export { Circle, Triangle, Flag, Info, Help }
